244dc7c37fefbfbc0d0d90712d35b634b682974c
proshian <proshian.itmo@gmail.com>
2025-03-31 18:38:52 +0300

    Add keyboard enabling in main MainActivity

--- app/src/main/java/com/example/neuralSwipeKeyboardProject/MainActivity.kt
+++ app/src/main/java/com/example/neuralSwipeKeyboardProject/MainActivity.kt
@@ -1,49 +1,58 @@
 package com.example.neuralSwipeKeyboardProject
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.neuralSwipeKeyboardProject.ui.theme.Executorch_neuroswipe_example_1Theme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         setContent {
-            Executorch_neuroswipe_example_1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Enable swipe typing in your keyboard settings and switch to it",
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp))
+
+                Button(
+                    onClick = {
+                        startActivity(
+                            Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)
+                                .apply { flags = Intent.FLAG_ACTIVITY_NEW_TASK }
+                        )
+                    }
+                ) {
+                    Text("Open Keyboard Settings")
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Executorch_neuroswipe_example_1Theme {
-        Greeting("Android")
     }
 }
